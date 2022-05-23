460b283d3deadc5401693b4abc4274f742105452
20373830 <1481304174@qq.com>
2022-05-23 21:49:28 +0800

    解决跨越

--- teen_renting/settings.py
+++ teen_renting/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders'
+    'corsheaders',
     'login_register',
     'django.contrib.admin',
     'django.contrib.auth',
