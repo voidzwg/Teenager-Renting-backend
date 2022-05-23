a425c3b08852167f563768689ebcaefce9d80d02
20373830 <1481304174@qq.com>
2022-05-23 13:27:19 +0800

    first

---
+++ login_register/models.py
@@ -0,0 +1,45 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Users(models.Model):
+    username = models.CharField(max_length=18)
+    password = models.CharField(max_length=18)
+    tel = models.CharField(max_length=11, blank=True, null=True)
+    email = models.CharField(max_length=30, blank=True, null=True)
+    name = models.CharField(max_length=30)
+    age = models.IntegerField(blank=True, null=True)
+    sex = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'users'
+
+
+class Workers(models.Model):
+    username = models.CharField(max_length=18)
+    password = models.CharField(max_length=18)
+    name = models.CharField(max_length=30)
+    tel = models.CharField(max_length=11, blank=True, null=True)
+    photo = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'workers'
+
+
+class Admins(models.Model):
+    name = models.CharField(max_length=30, blank=True, null=True)
+    username = models.CharField(max_length=18)
+    password = models.CharField(max_length=18)
+
+    class Meta:
+        managed = True
+        db_table = 'admins'
