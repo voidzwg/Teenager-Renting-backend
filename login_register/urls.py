4fa844d22ebafd3498c37455c0b235ea5407eead
20373830 <1481304174@qq.com>
2022-05-25 00:12:57 +0800

    "尝试pycharm的git"

--- login_register/urls.py
+++ login_register/urls.py
@@ -2,8 +2,6 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('', hello),
     path('login', login),
     path('register', register),
-    path('user/<int:id>/',detail)
 ]
