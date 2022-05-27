5f3ce89e7405bbff291cca7ff67d19124096709b
ljy <1481304174@qq.com>
2022-05-27 18:45:38 +0800

    新增browse_house应用

---
+++ browse_house/views.py
@@ -0,0 +1,27 @@
+import random
+from base64 import b64encode
+
+from django.http import JsonResponse
+
+from .models import *
+# Create your views here.
+def init(request):
+    house_list = Houses.objects.all()
+    data = []
+    for i in house_list[0:50]:
+        picture =  b64encode(i.pictures).decode('utf8')
+        floor_plan = b64encode(i.floor_plan).decode('utf8')
+        p_tmp = {
+            "id": i.id,
+            "short_price": i.short_price,
+            "long_price": i.long_price,
+            "area": i.area,
+            "location": i.location,
+            "type": i.type,
+            "available": i.available,
+            "floor_plan": floor_plan,
+            "picyures": picture,
+            "detail": i.details
+        }
+        data.append(p_tmp)
+    return JsonResponse(data,safe = False)
