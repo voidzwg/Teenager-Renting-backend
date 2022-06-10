39c1d707443ca73bd4b62d171f93b7d3857e3e03
Zhuang Weiguang <1454101799@qq.com>
2022-06-10 09:51:00 +0800

    定时任务

--- order/tasks.py
+++ order/tasks.py
@@ -1,11 +1,12 @@
-import logging
+from __future__ import absolute_import, unicode_literals
+from teen_renting.celery import app
 from celery import shared_task
 from datetime import datetime, timedelta, timezone
-from time import time
+from time import time, sleep
 from dateutil.relativedelta import relativedelta  # pip install python-dateutil
 from .models import Orders
+from com.funcs import my_send_email
 
-logger = logging.getLogger()
 tz = timezone(timedelta(hours=+8))
 
 
@@ -35,5 +36,26 @@ def send_email():
     now_date = datetime.fromtimestamp(int(time()), tz=tz)
     orders_list = Orders.objects.filter(status=1, type=1)
     for order in orders_list:
-        pass
+        n = order.duration
+        i = 0
+        start = order.start_time
+        end = start + relativedelta(months=n)
+        if start <= now_date <= end:
+            while i < n:
+                if now_date == start + relativedelta(months=i) + relativedelta(weeks=3)\
+                        and order.paid == 0:
+                    my_send_email([order.uid.email])
+                    return
+                i += 1
+
+
+@shared_task
+def test(x):
+    print("this is a test", x)
+    return "this is a test " + str(x)
+
+
+@shared_task
+def test_beat(string):
+    print("this is a test", string)
 
